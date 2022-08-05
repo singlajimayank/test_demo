8acaae85f146fa35ea0c9b045290c978f6cacfe4
singlajimayank <mayankbnl19@gmail.com>
2022-08-05 15:50:22 +0530

    set up view engine

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.posts = function(req,res){
+    return res.end('<h1> be the first one to like the post</h1>');
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+//use express router
 app.use('/',require('./routes'));
-
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
